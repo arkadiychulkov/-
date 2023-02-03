888bc7caa91da3902c175a8d0a30fcfcdd5b52d0
user <arkadiychulkov11@gmail.com>
2023-02-03 19:48:01 +0200

    фф

--- .idea/main.py
+++ .idea/main.py
@@ -4,13 +4,15 @@ class Stud:
         self.name = name
         self.hun = 50
         self.glad = 10
-        self.progres = 0
+        self.progres = 0.5
+        self.mon = 100
         self.alive = True
     def study(self):
         print("time to study")
         self.hun -= 3
         self.glad -= 3
-        self.progres += 0.15
+        self.progres += 0.5
+        self.mon -= 1
     def sleep(self):
         print("time to sleep")
         self.glad += 3
@@ -18,18 +20,39 @@ class Stud:
     def chill(self):
         self.glad += 5
         self.progres -= 0.1
+        self.hun += 2
+        self.mon -= 10
+    def work(self):
+        self.glad-= 1
+        self.progres -= 0.1
+        self.hun += 1
+        self.mon += 10
+
     def is_alive(self):
         if self.progres < 0.5:
            self.alive = False
            print("cast out")
         elif self.glad <= 0:
             print("ahhh i have depresion")
+        elif self.mon <= 0:
+            print("ahhh i don't have money")
+            self.alive = False
         elif self.progres > 5:
             print("passed")
-            self.alive = false
+            self.alive = False
     def day(self):
             print(f"progres", {self.progres})
             print(f"gladnes", {self.glad})
+            print(f"money", {self.mon})
+            if self.mon <= 80:
+                print('sssssssssssssssssssssssssss')
+                self.work()
+            if self.progres <= 3:
+                print('ychi ypoki debil')
+                self.study()
+                self.mon += 1
+            if self.glad <=10:
+                self.chill()
     def live(self, day):
             day = "Day"+str(day)+"of"+"live"
             print(f"{day:=^50}")
@@ -40,12 +63,17 @@ class Stud:
                 self.sleep()
             if cube == 3:
                 self.chill()
+
             self.day()
             self.is_alive()
+
+
 nick = Stud(name = "Nick")
 for day in range(365):
     if nick.alive == False:
+        print(nick.live(day))
         break
     nick.live(day)
+
 
 
